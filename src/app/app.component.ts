2bf6af5ade91521d125cdf1ff10719113016caa5
leaalex <macpro@mac-pro-mac.at.urfu.ru>
2017-02-16 09:27:35 +0500

    двухуровненвая структура страницы

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,10 @@ import { Component, OnInit  } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent  {
-<<<<<<< HEAD
+
   component:any = {
     name: 'AppComponent',
   };
   
-=======
- // title = 'app works!';
->>>>>>> b9463f29786624fa51ef4f6132aa39d6062a92c9
 
 }
