26858f92f8beb7aface567543e3d47a6bc4c9c06
MasterGowen <mastergowen@gmail.com>
2017-04-05 02:59:11 +0500

    remove document func

--- src/app/user-profile/load-materials/load-materials.component.ts
+++ src/app/user-profile/load-materials/load-materials.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Http, Response, Headers, RequestOptions } from '@angular/http';
-import { RegisterService} from 'app/services/register.service';
+import { RegisterService } from 'app/services/register.service';
 
 import { Observable } from 'rxjs/Observable';
 
@@ -11,58 +11,74 @@ import { Observable } from 'rxjs/Observable';
 })
 export class LoadMaterialsComponent implements OnInit {
 
-model: any = {};
-filesList: any[] = [];
-@Input() currentUser: any ;
+    model: any = {};
+    filesList: any[] = [];
+    @Input() currentUser: any ;
 
-constructor( private http: Http, private registerService: RegisterService) { }
+    constructor( private http: Http, private registerService: RegisterService) { }
 
-  ngOnInit() {
-    this.update();
-  }
-
-  update(){
-           this.registerService.getProfile().subscribe(userProfile => {
-            this.currentUser = userProfile;
-            console.log(userProfile);
-        });
-  }
-
-fileChange(event) {
-    console.log(event);
-    let fileList: FileList = event.target[0].files;
-    console.log(fileList);
-    if (fileList.length > 0) {
-        let file: File = fileList[0];
-        let formData: FormData = new FormData();
-        formData.append('uploadFile', file, file.name);
-       this.http.post('https://openedu.urfu.ru/edcrunch/api/v1/docs/upload/', formData, this.jwt())
-            .map(res => res.json())
-            .catch(error => Observable.throw(error))
-            .subscribe(
-                data => {console.log('success');  this.update() },
-                error => {console.log(error)}
-            );
+    ngOnInit() {
+        this.update();
     }
-}
-fileDelete(file_id: string){
-           this.http.post('https://openedu.urfu.ru/edcrunch/api/v1/docs/delete/', {'file_id' : file_id}, this.jwt())
-            .map(res => res.json())
-            .catch(error => Observable.throw(error))
-            .subscribe(
-                data => {console.log('success');  this.update() },
-                error => {console.log(error)}
-            );
-}
-    private jwt() {
-        // create authorization header with jwt token
-        let currentUser = JSON.parse(localStorage.getItem('currentUser'));
-        if (currentUser && currentUser.token) {
-            let headers = new Headers({ 'Authorization': currentUser.token,
-                                        'Accept': 'application/json',
-                                      });
-            return new RequestOptions({ headers: headers });
+
+    update() {
+            this.registerService.getProfile().subscribe(userProfile => {
+                this.currentUser = userProfile;
+            });
+    }
+
+    fileChange(event) {
+        console.log(event);
+        const fileList: FileList = event.target[0].files;
+        console.log(fileList);
+        if (fileList.length > 0) {
+            const file: File = fileList[0];
+            const formData: FormData = new FormData();
+            formData.append('uploadFile', file, file.name);
+        this.http.post('https://openedu.urfu.ru/edcrunch/api/v1/docs/upload/', formData, this.jwt())
+                .map(res => res.json())
+                .catch(error => Observable.throw(error))
+                .subscribe(
+                    data => { console.log('success');  this.update(); },
+                    error => { console.log(error); }
+                );
         }
     }
 
+    fileDelete(file_id: string) {
+        const body = { 'file_id': file_id };
+        const options = this.jwt();
+        options.headers.append('content-type', 'application/json');
+
+        this.http.post(
+            'https://openedu.urfu.ru/edcrunch/api/v1/docs/delete/',
+            JSON.stringify(body),
+            options
+            )
+        .toPromise()
+        .then(this.extractData)
+        .catch(this.handleError);
+    }
+
+    private extractData(res: Response) {
+        return res.status || 500;
+    }
+
+    private handleError (error: Response | any) {
+        let errMsg: string;
+        errMsg =  error.status || 500;
+        console.error('I can not delete this: ' + errMsg);
+        return Promise.resolve(errMsg);
+    }
+
+    private jwt() {
+        const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+        if (currentUser && currentUser.token) {
+            const headers = new Headers({
+                'Authorization': currentUser.token,
+                'Accept': 'application/json',
+            });
+            return new RequestOptions({ headers: headers });
+        }
+    }
 }
