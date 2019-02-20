553e12ec7c4b1e72cdb0a9ca6a9357e290b18865
MasterGowen <mastergowen@gmail.com>
2019-02-20 13:27:59 +0500

    2019#1

--- src/app/events/event-list/event-list.component.ts
+++ src/app/events/event-list/event-list.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { EventsService } from 'app/events/events.service';
-import { Router, Routes } from '@angular/router';
-import { Event } from 'app/events/event';
-import { Title } from '@angular/platform-browser';
-import { AuthGuard } from 'app/services/auth.guard';
-import { RegisterService} from 'app/services/register.service';
-import { AuthenticationService } from 'app/services/auth.service';
+import {Component, Input, OnInit} from '@angular/core';
+import {EventsService} from 'app/events/events.service';
+import {Router} from '@angular/router';
+import {Event} from 'app/events/event';
+import {Title} from '@angular/platform-browser';
+import {AuthGuard} from 'app/services/auth.guard';
+import {RegisterService} from 'app/services/register.service';
+import {AuthenticationService} from 'app/services/auth.service';
 
 @Component({
   selector: 'div.app-event-list',
@@ -28,7 +28,7 @@ export class EventListComponent implements OnInit {
   isLogged: boolean = false;
 
 
-@Input() typeFilter: string = '';
+  @Input() typeFilter: string = '';
 
 
   constructor(private router: Router,
@@ -37,58 +37,64 @@ export class EventListComponent implements OnInit {
               private authGuard: AuthGuard,
               private registerService: RegisterService,
               private authenticationService: AuthenticationService
-              ) {
+  ) {
   }
 
   ngOnInit() {
     this.isLogged = this.authGuard.canActivate();
-    this.title.setTitle("Мероприятия");
-        this.eventsService.getEventsList()
-          .subscribe(eventsList => {
-            this.eventsList = eventsList;
-            console.log(this.eventsList);
-            this.timeGrid = this.eventsService.getEventsObject(this.typeFilter);
-            this.registerService.getProfile().subscribe(
-              userProfile => { this.currentUser = userProfile; },
-              error => { this.authenticationService.logout(); }
-              );
+    this.title.setTitle('Мероприятия');
+    this.eventsService.getEventsList()
+      .subscribe(eventsList => {
+        this.eventsList = eventsList;
+        console.log(this.eventsList);
+        this.timeGrid = this.eventsService.getEventsObject(this.typeFilter);
+        this.registerService.getProfile().subscribe(
+          userProfile => {
+            this.currentUser = userProfile;
+          },
+          error => {
+            this.authenticationService.logout();
+          }
+        );
         }
       );
   }
 
   ngOnChanges(changes: any) {
-      if (this.eventsList) {
-        this.timeGrid = this.eventsService.getEventsObject(this.typeFilter);
-      }
+    if (this.eventsList) {
+      this.timeGrid = this.eventsService.getEventsObject(this.typeFilter);
+    }
   }
 
-  funcSelectedDay(value){
+  funcSelectedDay(value) {
     this.uniqueTimes = this.eventsService.getUniqueTimesByDay(value);
   }
 
-  funcSelectedTime(value){
+  funcSelectedTime(value) {
     this.currentEvents = this.eventsService.getEventsByDayTimes(value);
 
   }
 
   public toEvent(event: Event) {
-        this.router.navigate(["events", "event", event.id]);
-    }
+    this.router.navigate(['events', 'event', event.id]);
+  }
 
- // передаем список событий
- // получаем отсортированный список дат, когда есть события
-  getUniqueDates(eventsList:Event[]){
+  // передаем список событий
+  // получаем отсортированный список дат, когда есть события
+  getUniqueDates(eventsList: Event[]) {
     let uniqueDates: Date[] = [];
-        for(let item of eventsList){
-          let event: Event = item;
-            if(!uniqueDates.find(item => item.getDate() === event.startdate.getDate() && item.getMonth() === event.startdate.getMonth() )){
-                uniqueDates.push(event.startdate);
-            }
-            /*if(!uniqueDates.find(item => item.getTime() == event.startdate.getTime() )){
-                uniqueDates.push(event.startdate);
-            }*/
-        }
-    uniqueDates.sort(function(a,b){return a.getTime() - b.getTime()});
+    for (let item of eventsList) {
+      let event: Event = item;
+      if (!uniqueDates.find(item => item.getDate() === event.startdate.getDate() && item.getMonth() === event.startdate.getMonth())) {
+        uniqueDates.push(event.startdate);
+      }
+      /*if(!uniqueDates.find(item => item.getTime() == event.startdate.getTime() )){
+          uniqueDates.push(event.startdate);
+      }*/
+    }
+    uniqueDates.sort(function (a, b) {
+      return a.getTime() - b.getTime()
+    });
     return uniqueDates;
   }
 
--- src/app/user-service.service.spec.ts
+++ src/app/user-service.service.spec.ts
@@ -1,16 +1,16 @@
-/* tslint:disable:no-unused-variable */
-
-import { TestBed, async, inject } from '@angular/core/testing';
-import { UserServiceService } from './user-service.service';
-
-describe('UserServiceService', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [UserServiceService]
-    });
-  });
-
-  it('should ...', inject([UserServiceService], (service: UserServiceService) => {
-    expect(service).toBeTruthy();
-  }));
-});
+// /* tslint:disable:no-unused-variable */
+//
+// import { TestBed, async, inject } from '@angular/core/testing';
+// import { UserServiceService } from './user-service.service';
+//
+// describe('UserServiceService', () => {
+//   beforeEach(() => {
+//     TestBed.configureTestingModule({
+//       providers: [UserServiceService]
+//     });
+//   });
+//
+//   it('should ...', inject([UserServiceService], (service: UserServiceService) => {
+//     expect(service).toBeTruthy();
+//   }));
+// });
