6f458264e9adad86849ec0110656f4ddf08f520f
vesloguzov <vesloguzov@gmail.com>
2019-03-25 16:45:17 +0500

    translate service #7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,9 +32,12 @@ constructor(private _location: Location,
   }
 
   ngOnInit() {
-  this._translate.setDefaultLang("en");
+    this._translate.setDefaultLang("en");
   // console.log(navigator.language, window.navigator['userLanguage']);
-   let lang = navigator.language || window.navigator['userLanguage'];
+    let lang = navigator.language || window.navigator['userLanguage'];
+    if (lang.includes('ru')){
+      lang = 'ru';
+    }
     // console.log("navigator.language", navigator.language);
     // console.log("this._translate", this._translate);
     this._translate.use(lang);
